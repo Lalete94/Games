03d46ee5f6c1c31c8ae1286250b48cf18b5ed1d1
lalete <afpg.1819@gmail.com>
2024-04-22 23:28:06 -0400

    segunda pre-entrega

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
+let productos = [
+    { id: 2, nombre: "God of War", categoria: "Accion", stock: 2, precio: 55000, rutaimagen: "God-of-war.jpg"},
+    { id: 5, nombre: "Fifa24", categoria: "Deportes", stock: 7, precio: 50000, rutaimagen: "fifa.jpg"},
+    { id: 7, nombre: "Resident Evil 7", categoria: "Terror", stock: 4, precio: 25000, rutaimagen: "resident-evil.jpg"},
+    { id: 9, nombre: "The last uf as", categoria: "Terror", stock: 1, precio: 30000, rutaimagen: "The.jpg"},
+    { id: 12, nombre: "Call of duty", categoria: "Accion", stock: 3, precio: 35000, rutaimagen: "call.jpg"},
+]
 
-let mensaje = "Ingrese una opcion:\n1 - Agregar al carrito\n2 - Consultar total\n3 - Terminar compra\n0 - Salir"
-let opcion
-let producto = "Selecciona un producto\n1 - God Of War $55000\n2 - Fifa $50000\n3 - Resident Evil 7 $25000\n4 - Volver"
-let total = 0
+
+
+const listar = (lista, propiedad1, propiedad2) => lista.map(producto => producto[propiedad1] + " - " + producto[propiedad2]).join("\n")
+
+let carrito = []
+let opcionMenu
 
 do {
-    opcion = Number(prompt(mensaje))
-    if (opcion === 1) {
-        total = agregarProductosAlCarrito(total)
-    } else if (opcion === 2) {
-        alert("Su total hasta ahora es: " + "$" + total)
-    } else if (opcion === 3) {
-        alert("Su compra total es de: " + "$" + total + "\n¿Desea concretar la compra?")
-        alert("Su compra ha sido realizada con exito." + "\n¡Gracias por preferirnos!")
-        total = 0
+    opcionMenu = Number(prompt("1 - Agregar al carro\n2 - Filtrar por categoria\n0 - Salir"))
+    if (opcionMenu === 1) {
+        agregarAlCarrito (productos, carrito)
+    }else if (opcion === 2){
+        filtrarCategoria(productos)
     }
-} while (opcion !== 0 && opcion !== 3)
+} while (opcionMenu !== 0);
 
-function agregarProductosAlCarrito(total) {
-    let opcionJuegos = Number(prompt(producto))
-    if (opcionJuegos === 1) {
-        let cantidad = Number(prompt("Ingrese cantidad en unidad"))
-        total = total + cantidad * 55000
-    } else if (opcionJuegos === 2) {
-        let cantidad = Number(prompt("Ingrese cantidad en unidad"))
-        total = total + cantidad * 50000
-    } else if (opcionJuegos === 3) {
-        let cantidad = Number(prompt("Ingrese cantidad en unidad"))
-        total = total + cantidad * 25000
+function filtrarCategoria(productos){
+    let categoria = prompt("Seleccione Categoria")
+    let productosFiltrados = productos.filter(producto => producto.categoria === categoria)
+    let salida = productosFiltrados.map(producto => producto.nombre).join("\n")
+    alert(salida)
+}
+
+function agregarAlCarrito(productos, carrito) {
+opcion = Number(prompt(listar(productos, "id", "nombre")))
+
+    let buscarProducto = productos.find(producto => producto.id === opcion)
+    let posicionProducto = carrito.findIndex(producto => producto.id === opcion)
+
+    if(buscarProducto){
+        if(posicionProducto !== -1){
+            carrito[posicionProducto].unidades++
+            carrito[posicionProducto].subtotal = carrito [posicionProducto].precioUnidad * carrito[posicionProducto].unidades
+        } else {
+            carrito.push({
+                id: buscarProducto.id,
+                nombre: buscarProducto.nombre,
+                precioUnidad: buscarProducto.precio,
+                unidades: 1,
+                subtotal: buscarProducto.precio
+            })
+        }
+    } else {
+        alert("ID incorrecto")
     }
-    return total
 }
